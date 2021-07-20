c3a74362f9b58902096a0139d9ae659ab2b1f097
me <04igorignatev@gmail.com>
2021-07-20 20:18:05 +0300

    mod: -class C_Calc_Fuel

--- ConsoleApplication1/ConsoleApplication1/C_Calc_Fuel.cpp
+++ ConsoleApplication1/ConsoleApplication1/C_Calc_Fuel.cpp
@@ -1,12 +1,11 @@
 #include "C_Calc_Fuel.h"
 
-calc_fuel::C_Calc_Fuel::C_Calc_Fuel(float _litres, float _distance) : C_Calc(_litres, distance)
+calc_fuel::C_Calc_Fuel::C_Calc_Fuel(float _litres, float _distance) : C_Calc(_distance)
 {
-	this->distance = _distance;
 	this->_litres = _litres;
 }
 
 float calc_fuel::C_Calc_Fuel::calc()
 {
-	return this->_litres / this->distance;
+	return (this->_litres / this->distance) * 100;
 }
